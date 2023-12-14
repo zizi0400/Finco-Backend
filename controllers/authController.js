dcf7f843acb2940def4de6354152bab45d4cee47
dci-student <zizi.0400@gmail.com>
2023-12-14 15:29:36 +0100

    Authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,12 +6,10 @@ export const register = async (req, res) => {
   if (user) {
     res.status(500).end("User is ready");
   } else {
-    await db
-      .collection("users")
-      .insertOne({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      });
+    await db.collection("users").insertOne({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
   }
 };
